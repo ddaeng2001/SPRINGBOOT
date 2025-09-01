dea0d0bc615d797f357626dbb1ffc178e186ea7f
ddaeng2001 <ddaeng2001@naver.com>
2025-09-01 17:26:30 +0900

    09/01

---
+++ 02_LOMBOK_DI/src/main/java/com/example/demo/Dto/PersonDto.java
@@ -0,0 +1,23 @@
+package com.example.demo.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*@Getter
+@Setter
+@ToString*/
+@NoArgsConstructor //디폴트 생성자
+@AllArgsConstructor
+@Data
+//@Data로 get,set,equals,toString()등을 사용할 수 있음
+@Builder
+
+// f == fields, m == 메서드
+public class PersonDto {
+    private String name;
+    private int age;
+    private String addr;
+
+}
