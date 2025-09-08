d22ba39184f713754cb5185307235869a17a78fe
ddaeng2001 <ddaeng2001@naver.com>
2025-09-08 17:50:13 +0900

    09/08

--- 0909모듈테스트/src/main/java/com/example/demo/domain/repository/MemoRepository.java
+++ 0909모듈테스트/src/main/java/com/example/demo/domain/repository/MemoRepository.java
@@ -1,11 +1,12 @@
 package com.example.demo.domain.repository;
 
 import com.example.demo.domain.entity.Memo;
-//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface MemoRepository extends JpaRepository<Memo,Long> {
-
+                                                    //<엔터티, 엔터티 pk 자료형(타입)>
+                                        //JpaRepository<Memo,Long> : 기본 CRUD하는 함수들이 생성됨
 }
